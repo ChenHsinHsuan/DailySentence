1b6d8f7e7dca41ed6569b52da05d73d0ce4d3628
ChenHsinHsuan <aircon.chen@gmail.com>
2015-05-17 20:44:44 +0800

    完成

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Google-Mobile-Ads-SDK
+#define COCOAPODS_POD_AVAILABLE_Google_Mobile_Ads_SDK
+#define COCOAPODS_VERSION_MAJOR_Google_Mobile_Ads_SDK 7
+#define COCOAPODS_VERSION_MINOR_Google_Mobile_Ads_SDK 2
+#define COCOAPODS_VERSION_PATCH_Google_Mobile_Ads_SDK 2
+
+// KDEAudioPlayer
+#define COCOAPODS_POD_AVAILABLE_KDEAudioPlayer
+#define COCOAPODS_VERSION_MAJOR_KDEAudioPlayer 0
+#define COCOAPODS_VERSION_MINOR_KDEAudioPlayer 2
+#define COCOAPODS_VERSION_PATCH_KDEAudioPlayer 0
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
+
+// Spring
+#define COCOAPODS_POD_AVAILABLE_Spring
+#define COCOAPODS_VERSION_MAJOR_Spring 1
+#define COCOAPODS_VERSION_MINOR_Spring 0
+#define COCOAPODS_VERSION_PATCH_Spring 3
+
